45bdc6bd1c03391341a6fb2143af0045b84c818e
man7788 <jerman1841@gmail.com>
2022-09-11 01:58:48 +0800

    Finish all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  splitWords = string.split('');
+  reverseWords = splitWords.reverse();
+  joinedWords = reverseWords.join('');
+  return joinedWords;
 };
 
 // Do not edit below this line
