b503c2e130a6ac25e2ea7c45e3de6bc517b6a63b
man7788 <jerman1841@gmail.com>
2022-09-11 06:50:06 +0800

    Finish all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahr, cel) { // [°C] = ([°F] − 32) × 5⁄9
+  celsius = Math.round((fahr - 32) * (5/9) * 10) / 10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(cel, fahr) { // [°F] = [°C] × 9⁄5 + 32	
+  fahrenheit = Math.round((cel * (9/5) + 32) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
