3ab506da86e7a30eb620761cc3f3004f3be8cf21
man7788 <jerman1841@gmail.com>
2022-09-11 04:59:52 +0800

    Finish all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbers, ...remove) {
+  const numArray = numbers;
+  const rmArray = remove;
 
+  function predicate(value) {
+    if (!rmArray.includes(value)) {
+      return value;
+    }
+  }
+  const filtered = numArray.filter(predicate);
+  return filtered;
 };
 
 // Do not edit below this line
