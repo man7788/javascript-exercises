3c535cae244e9a1633899a4da7f5aa63fafd5e85
man7788 <jerman1841@gmail.com>
2022-09-11 06:16:19 +0800

    Finish all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let sumNum = 0;
+  let sumTimes = num1 + num2;
+  let start;
+  if (num1 < 0 || num2 < 0) { 
+    return 'ERROR';
+  }
+  if (!Number.isFinite(num1) || !Number.isFinite(num2)) {
+    return 'ERROR'
+  }
+  if (num1 > num2) {
+    start = num2;
+  } else {
+    start = num1;
+  }
+  for (i = start; i < sumTimes; i++) {
+    sumNum += i;
+  } 
+  return sumNum;
 };
 
 // Do not edit below this line
