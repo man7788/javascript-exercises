08c2924aa8b9424e42e81a979bbfed55e9809c2a
man7788 <jerman1841@gmail.com>
2022-09-11 01:43:11 +0800

    Finish all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const number = Math.floor(Math.random() * 1000)
 
+const repeatString = function(string, num) {
+  let threeTimes = string;
+  if (num < 0) {
+    threeTimes = 'ERROR';
+  }
+  if (num === 0) {
+    threeTimes = '';
+  }
+  for (i = 0; i < num-1; i++) {
+    threeTimes += string;
+  };
+  return threeTimes;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
