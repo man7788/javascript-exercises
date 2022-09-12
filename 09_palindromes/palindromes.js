9a8b6d3f5da51e92bcbd4f17313d2f10b1a12799
man7788 <jerman1841@gmail.com>
2022-09-13 03:12:59 +0800

    Finish all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let text = string.toLowerCase();
+  stripText = text.replace(/\W/g, '');
+  splitString = stripText.split("");
+  reverseArrary = splitString.reverse();
+  joinArray = reverseArrary.join("");
+  return (joinArray === stripText);
 };
 
 // Do not edit below this line
