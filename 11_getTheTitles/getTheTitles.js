d521274b1d8fcdea21c184e20dd46b83682d79e6
man7788 <jerman1841@gmail.com>
2022-09-13 05:21:28 +0800

    Finish all tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(item) {
+  const bookTitle = [];
+  for (i = 0; i < item.length; i++) {
+    titleName = item[i].title;
+    bookTitle.push(titleName);
+  }
+  return bookTitle;
 };
 
 // Do not edit below this line
