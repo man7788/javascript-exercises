7b872a62672b9763d095159f20f6499b8e86acb7
man7788 <jerman1841@gmail.com>
2022-09-13 04:59:53 +0800

    Finish all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(range) {
+  if (range < 0) {
+    return 'OOPS'
+  }
+  let numArray = [0, 1];
+  for (i = 1; i < range; i++) { 
+  let nextTerm = numArray[numArray.length-1] + numArray[numArray.length-2];
+  numArray.push(nextTerm);
+  }
+  console.log(numArray);
+  return numArray[numArray.length-1];
+ 
 };
 
 // Do not edit below this line
