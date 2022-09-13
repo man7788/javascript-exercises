51640dabb0044b4f90d90c95ceedea6905b657a5
man7788 <jerman1841@gmail.com>
2022-09-13 08:27:01 +0800

    Finish all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(item) {
+  const ageArray = [];
+  let largest = null;
+  let indexNum;
 
+  let presentYear = new Date();
+  presentYear = presentYear.getFullYear();
+
+
+  for (i = 0; i < item.length; i++) {
+    if (item[i].yearOfDeath === undefined) {
+      item[i].yearOfDeath = presentYear
+    }
+    let age = item[i].yearOfDeath - item[i].yearOfBirth;
+    ageArray.push(age);
+    }
+    for (i = 0; i < ageArray.length; i++) {
+      if (ageArray[i] === null || ageArray[i] > largest) {
+        largest = ageArray[i];
+        indexNum = i;
+    }
+  };
+  console.log(item[0].yearOfDeath);
+  console.log(ageArray)
+  console.log(presentYear);
+
+  return item[indexNum];
 };
 
 // Do not edit below this line
